e4a2f63f9efa7087365b2ff247394d0d3b9f5ed4
Timothy A <albantimothy@gmail.com>
2024-01-21 10:28:34 -0500

    Updated status

--- src/main/java/org/crawler/CrawlingManager.java
+++ src/main/java/org/crawler/CrawlingManager.java
@@ -3,14 +3,23 @@ package org.crawler;
 import org.scraper.drivers.ScrapeManager;
 import org.scraper.page.Page;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Queue;
+import java.util.TreeSet;
+
 public class CrawlingManager {
     Page currentPage;
 
+    private CrawlingQueue<HashMap<String, List<String>>> urlsToCrawl;
     CrawlingManager(){
         currentPage = new Page();
+        urlsToCrawl = new CrawlingQueue();
     }
     public void startCrawling(String url){
         currentPage.setBaseUrl(url);
         currentPage.scrape();
     }
+    
+
 }
---
+++ src/main/java/org/crawler/CrawlingQueue.java
@@ -0,0 +1,15 @@
+package org.crawler;
+
+import net.bytebuddy.implementation.bytecode.Throw;
+
+import java.util.*;
+
+public class CrawlingQueue<T> extends ArrayDeque<T>  {
+
+
+    public CrawlingQueue(){
+        super();
+    }
+
+
+}
